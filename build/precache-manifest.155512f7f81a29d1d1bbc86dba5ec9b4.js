b86aa845c4f834b27318753f330653a374ca9b25
Aleksander <matikainen.aleksander@gmail.com>
2020-07-03 19:24:05 +0300

    frontend was added

---
+++ build/precache-manifest.155512f7f81a29d1d1bbc86dba5ec9b4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1a45c446ef9f344e4f78635020e38ea",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5830e4015856b2197c26",
+    "url": "/static/css/main.ac05f873.chunk.css"
+  },
+  {
+    "revision": "ea7dfe846377567f1a2f",
+    "url": "/static/js/2.2de4c12b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.2de4c12b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5830e4015856b2197c26",
+    "url": "/static/js/main.685b2331.chunk.js"
+  },
+  {
+    "revision": "f0be2d6b237b3fb64219",
+    "url": "/static/js/runtime-main.24b8e7b7.js"
+  }
+]);
